a37a00c2c21620a6b7b69bfa55524dd48bca11f8
Sebastian Vendt <sebastian.vendt@uni-ulm.de>
2019-01-21 13:26:50 +0100

    First logging on console of touch event

--- app/src/main/java/com/motionlogger/sebastianvendt/motionlogger/MainActivity.java
+++ app/src/main/java/com/motionlogger/sebastianvendt/motionlogger/MainActivity.java
@@ -1,5 +1,8 @@
 package com.motionlogger.sebastianvendt.motionlogger;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,14 +11,31 @@ import android.view.View;
 import android.widget.EditText;
 import android.content.Intent;
 
+import java.util.Iterator;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private SensorManager mSensorManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(android.R.id.content).setOnTouchListener(handleTouch);
+
+        //list the sensors available on the device
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        Iterator<Sensor> sensorIterator = deviceSensors.iterator();
+
+        while (sensorIterator.hasNext()){
+            Sensor sens = sensorIterator.next();
+            EditText console = (EditText) findViewById(R.id.Console);
+            console.setText(sens.getName());
+        }
+
 
 
 
